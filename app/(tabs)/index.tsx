2ad7a6809127eabf58af5f049f1828918885ad9c
RohitSalgar <rohitadinathsalgar@allmasters.ai>
2024-12-28 20:47:28 +0530

    dashboard changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,36 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, TextInput } from "react-native";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import Entypo from "@expo/vector-icons/Entypo";
 
 const Index = () => {
   return (
     <View style={styles.container}>
-      <Text>Dashboard</Text>
+      <View style={styles.profileWrapper}>
+        <View style={styles.profileContainer}>
+          <View style={styles.textContainer}>
+            <View style={styles.profilePic}></View>
+            <View>
+              <Text style={styles.propertyName}>
+                All Properties
+                <Entypo name="chevron-down" size={20} color="#fff" />
+              </Text>
+            </View>
+          </View>
+          <View style={styles.optionsContainer}>
+            <View>
+              <MaterialIcons name="notification-add" size={24} color="#fff" />
+            </View>
+            <View>
+              <MaterialCommunityIcons name="menu" size={24} color="#fff" />
+            </View>
+          </View>
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.inputText} />
+        </View>
+      </View>
     </View>
   );
 };
@@ -13,6 +39,61 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 50,
+  },
+  profileWrapper: {
+    width: "100%",
+    height: 150,
+    backgroundColor: "#256D85",
+    paddingHorizontal: 20,
+  },
+  profileContainer: {
+    width: "100%",
+    display: "flex",
+    marginTop: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  profilePic: {
+    width: 40,
+    height: 40,
+    backgroundColor: "#fff",
+    borderRadius: 50,
+  },
+  textContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: 10,
+  },
+  optionsContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: 10,
+  },
+  propertyName: {
+    fontSize: 16,
+    color: "#fff",
+  },
+  inputContainer: {
+    marginTop: 5,
+  },
+  inputText: {
+    width: "100%",
+    height: 50,
+    borderWidth: 1,
+    borderColor: "#678d9a",
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    fontSize: 18,
+    lineHeight: 20,
+    marginVertical: 10,
+    backgroundColor: "#678d9a",
+    color: "#fff",
   },
 });
 
